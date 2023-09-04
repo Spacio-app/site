5eb27e122e986d3bdf6437fe9edcb7c56dbee9e4
CrisAlvar3z <cris.alvarez.tobal@gmail.com>
2023-09-04 14:45:57 -0300

    Creating signin view

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,12 +1,54 @@
 import GoogleLoginButton from '@/app/components/GoogleSignInButton'
-
+import '@/app/(site)/globals.css'
+import Image from 'next/image'
 const SignIn = async () => {
   return (
-      <section>
-        <div className='center py-4'>
-          <h1 className='text-2xl font-semibold tracking-tight'>Inicio de sesion</h1>
+      <section className='h-full'>
+        <div className='flex items-center h-full bg-gray-400'>
+          <div className='flex-auto max-w-[50%]'>
+            <Image
+              className='m-auto'
+              src={'/login_door.svg'}
+              alt='logo'
+              height={400}
+              width={400}
+            />
+          </div>
+          <div className='flex-auto max-w-[50%] h-full bg-white flex items-center'>
+            <div className="w-full">
+              <div>
+                <h1 className='text-2xl text-center font-semibold tracking-tight'>Bienvenido!</h1>
+              </div>
+              <form className="bg-white rounded px-20 pt-6 pb-8 mb-4">
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                    Username
+                  </label>
+                  <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+                </div>
+                <div className="mb-6">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                    Password
+                  </label>
+                  <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+                  <p className="text-red-500 text-xs italic">Please choose a password.</p>
+                </div>
+                <div className="flex items-center justify-center gap-4">
+                  <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline" type="button"
+                    style={{ width: '118px' }}
+                  >
+                    Sign In
+                  </button>
+                  <GoogleLoginButton />
+                </div>
+              </form>
+              <p className="text-center text-gray-500 text-xs">
+                &copy;2020 Acme Corp. All rights reserved.
+              </p>
+            </div>
+          </div>
         </div>
-        <GoogleLoginButton />
       </section>
   )
 }
--- src/app/components/GoogleSignInButton.tsx
+++ src/app/components/GoogleSignInButton.tsx
@@ -11,31 +11,31 @@ const GoogleLoginButton = () => {
   //   console.log('callbackUrl', callbackUrl)
   // }, [callbackUrl])
 
-  return (
+  return (<>
     <button
-      className='w-full'
+      className='bg-red-700 hover:bg-red-500 text-white font-bold py-2 px-4 rounded-full text-center'
+      style={{ width: '118px' }}
       onClick={() => {
         void signIn('google', { callbackUrl: 'http://localhost:3000/' })
-      }
-      }
+      }}
     >
       <svg
         aria-hidden='true'
         focusable='false'
         data-icon='google'
         className='mr-8 w-5'
+        style={{ marginRight: 'auto', marginLeft: 'auto' }}
         role='img'
         xmlns='http://www.w3.org
         /2000/svg'
         viewBox='0 0 488 512'
       >
         <path
-          fill='red'
+          fill='white'
           d='M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z'
         ></path>
       </svg>
-      Continue with Google
-    </button>
+    </button></>
   )
 }
 
