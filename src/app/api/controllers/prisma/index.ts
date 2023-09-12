f85be9f82928e43d24c4cc6e8b4733b1aa678ebf
CrisAlvar3z <cris.alvarez.tobal@gmail.com>
2023-09-12 13:36:37 -0300

    Adding next-on-page package

--- src/app/api/controllers/prisma/index.ts
+++ src/app/api/controllers/prisma/index.ts
@@ -2,7 +2,4 @@ import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 const prisma = new PrismaClient().$extends(withAccelerate())
-
-// if (process.env.NODE_ENV !== 'production') globalThis.prisma = client
-
 export default prisma
